d201fc0c98fba9b0fa4401e45a394d38a1b21c90
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-11-06 20:56:56 -0700

    Actually do a search, though don't do anything with the data yet

--- ingestr-server/ingestr.py
+++ ingestr-server/ingestr.py
@@ -1,15 +1,27 @@
 from __future__ import division, absolute_import
 from flask import Flask, render_template, request
+import json
+import urllib2
 
 # CONFIG
 SECRET_KEY = 'super seekrit'
+DOCUMENT_URL_PATTERN = 'http://localhost:9200/{index}/item/{item}'
+SEARCH_URL_PATTERN = 'http://localhost:9200/_search?q={query}'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
 def search():
-    return render_template('search.html', query = request.args.get('query'))
+    args = {'url': None, 'data': None}
+    if request.args.get('query', False):
+        args['url'] = app.config['SEARCH_URL_PATTERN'].format(query = request.args.get('query'))
+        req = urllib2.Request(args['url'])
+        opener = urllib2.build_opener()
+        f = opener.open(req)
+        args['data'] = json.load(f)
+    print repr(args)
+    return render_template('search.html', query = args['url'], data = args['data'])
 
 @app.route('/<index>/<item>')
 def document(index, item):
