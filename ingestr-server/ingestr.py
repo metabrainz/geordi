8eff84a891b3803c14b5c5db6289498764a74bfb
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-11-06 17:49:16 -0700

    Clean up the URL routing/templates, let's keep this to two pages.

--- ingestr-server/ingestr.py
+++ ingestr-server/ingestr.py
@@ -8,16 +8,12 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def search():
+    return render_template('search.html', query = request.args.get('query'))
 
 @app.route('/<index>/<item>')
 def document(index, item):
     return render_template('document.html', item=item, index=index)
-
-@app.route('/search')
-def search():
-    return render_template('search.html', query = request.args.get('query'))
 
 if __name__ == '__main__':
     app.run(debug = True)
