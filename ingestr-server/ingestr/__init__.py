f7d718ac232b9ba80b7ec908d843e69680837588
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-11-30 21:58:13 -0700

    purge remaining DB-related code

--- ingestr-server/ingestr/__init__.py
+++ ingestr-server/ingestr/__init__.py
@@ -5,9 +5,6 @@ from flask import Flask
 SECRET_KEY = 'super seekrit'
 DOCUMENT_URL_PATTERN = 'http://localhost:9200/{index}/item/{item}'
 SEARCH_URL_PATTERN = 'http://localhost:9200/_search?q={query}&size=10&from={start_from}'
-DB_NAME = 'ingestr'
-DB_USER = 'ingestr'
-DB_HOST = 'localhost'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
