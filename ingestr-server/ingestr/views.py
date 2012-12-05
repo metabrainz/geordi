9ccb8fd17aaaba41dce1b66d3b09f4b12251b6db
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-12-05 12:12:11 -0700

    Add basic stubbed-out api views

--- ingestr-server/ingestr/views.py
+++ ingestr-server/ingestr/views.py
@@ -15,14 +15,16 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import division, absolute_import
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, Response
 from flask.ext.login import login_required, login_user, logout_user
 from ingestr import app, login_manager, User
 from ingestr.search import do_search
 
 import json
+import uuid
 from pyelasticsearch import ElasticSearch, ElasticHttpNotFoundError
 
+# Main user-facing views
 @app.route('/')
 def search():
     data = None
@@ -40,6 +42,43 @@ def document(index, item):
     except ElasticHttpNotFoundError:
         return render_template('notfound.html')
 
+# Internal API urls for matching etc.
+@app.route('/api/matchitem/<index>/<item>')
+@login_required
+def matchitem(index, item):
+    "Submit a match for this item"
+    matchtype = request.args.get('type', 'release')
+    mbid = request.args.get('mbid', None)
+    if not mbid:
+        return Response(json.dumps({'code': 400, 'error': 'You must provide an MBID for a match.'}), 400)
+    try:
+        uuid.UUID('{{{uuid}}}'.format(uuid=mbid))
+    except ValueError:
+        return Response(json.dumps({'code': 400, 'error': 'The provided MBID is ill-formed'}), 400)
+
+    pass
+
+@app.route('/api/subitem/<index>/<subitem>')
+def subitem(index, subitem):
+    "Get information for a subitem's matching"
+    pass
+
+@app.route('/api/matchsubitem/<index>/<subitem>')
+@login_required
+def matchsubitem(index, subitem):
+    "Submit a match for this subitem"
+    matchtype = request.args.get('type', 'release')
+    mbid = request.args.get('mbid', None)
+    if not mbid:
+        return Response(json.dumps({'code': 400, 'error': 'You must provide an MBID for a match.'}), 400)
+    try:
+        uuid.UUID('{{{uuid}}}'.format(uuid=mbid))
+    except ValueError:
+        return Response(json.dumps({'code': 400, 'error': 'The provided MBID is ill-formed'}), 400)
+
+    pass
+
+# Login/logout-related views
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST" and "username" in request.form:
