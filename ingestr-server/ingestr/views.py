997f0fc7003cf2cb07239fd36b8dce4cc5325cd1
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-12-05 14:25:01 -0700

    Change to using real login by way of shamelessly copying acoustid-server's implementation

--- ingestr-server/ingestr/views.py
+++ ingestr-server/ingestr/views.py
@@ -22,7 +22,11 @@ from ingestr.search import do_search
 
 import json
 import uuid
+import urllib2
+
 from pyelasticsearch import ElasticSearch, ElasticHttpNotFoundError
+
+standard_options = {'json': json, 'all_indices': app.config['AVAILABLE_INDICES']}
 
 # Main user-facing views
 @app.route('/')
@@ -31,16 +35,16 @@ def search():
     start_from = request.args.get('from', '0')
     if request.args.get('query', False):
         data = do_search(request.args.get('query'), request.args.getlist('index'))
-    return render_template('search.html', query = request.args.get('query'), data = data, json = json, start_from = start_from, all_indices = app.config['AVAILABLE_INDICES'], indices = request.args.getlist('index'))
+    return render_template('search.html', query = request.args.get('query'), data = data, start_from = start_from, indices = request.args.getlist('index'), **standard_options)
 
 @app.route('/<index>/<item>')
 def document(index, item):
     es = ElasticSearch(app.config['ELASTICSEARCH_ENDPOINT'])
     try:
         data = es.get(index, 'item', item)
-        return render_template('document.html', item=item, index=index, data = data, json = json, all_indices = app.config['AVAILABLE_INDICES'])
+        return render_template('document.html', item=item, index=index, data = data, **standard_options)
     except ElasticHttpNotFoundError:
-        return render_template('notfound.html')
+        return render_template('notfound.html', **standard_options)
 
 # Internal API urls for matching etc.
 @app.route('/api/matchitem/<index>/<item>')
@@ -83,10 +87,17 @@ def matchsubitem(index, subitem):
 def login():
     if request.method == "POST" and "username" in request.form:
         username = request.form["username"]
-        login_user(User(username))
-        flash("Logged in!")
-        return redirect(request.args.get("next") or url_for("search"))
-    return render_template("login.html", all_indices = app.config['AVAILABLE_INDICES'])
+        password = request.form["password"]
+        if username and password:
+            if check_mb_account(username, password):
+                login_user(User(username))
+                flash("Logged in!")
+                return redirect(request.args.get("next") or url_for("search"))
+            else:
+                flash('Invalid username or password.')
+        else:
+            flash('You must provide a username and password.')
+    return render_template("login.html")
 
 @app.route("/logout")
 @login_required
@@ -94,3 +105,32 @@ def logout():
     logout_user()
     flash("Logged out.")
     return redirect(url_for("search"))
+
+# What follows, until the end of the file, is shamelessly cribbed from acoustid-server
+class DigestAuthHandler(urllib2.HTTPDigestAuthHandler):
+    """Patched DigestAuthHandler to correctly handle Digest Auth according to RFC 2617.
+
+    This will allow multiple qop values in the WWW-Authenticate header (e.g. "auth,auth-int").
+    The only supported qop value is still auth, though.
+    See http://bugs.python.org/issue9714
+
+    @author Kuno Woudt
+    """
+    def get_authorization(self, req, chal):
+        qop = chal.get('qop')
+        if qop and ',' in qop and 'auth' in qop.split(','):
+            chal['qop'] = 'auth'
+        return urllib2.HTTPDigestAuthHandler.get_authorization(self, req, chal)
+
+def check_mb_account(username, password):
+    url = 'https://musicbrainz.org/ws/2/artist/89ad4ac3-39f7-470e-963a-56509c546377?inc=user-tags'
+    auth_handler = DigestAuthHandler()
+    auth_handler.add_password('musicbrainz.org', 'https://musicbrainz.org/',
+                              username.encode('utf8'), password.encode('utf8'))
+    opener = urllib2.build_opener(auth_handler)
+    opener.addheaders = [('User-Agent', 'Ingestr-Login +http://ingestr.musicbrainz.org/login')]
+    try:
+        opener.open(url, timeout=5)
+    except StandardError:
+        return False
+    return True
