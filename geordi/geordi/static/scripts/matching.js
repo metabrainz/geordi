5c00762427b8a5c218e7bb27f7f7c7ae2e2a64b6
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2012-12-14 17:46:59 -0700

    Reword, fix bug where first match was being used rather than the current one

--- geordi/geordi/static/scripts/matching.js
+++ geordi/geordi/static/scripts/matching.js
@@ -12,22 +12,24 @@ $('.item-linker').click(function(e) {
 // AJAX form submission
 $('form.match-form').submit(function (e) {
   e.preventDefault();
-  $.ajax({
+  var promise = $.ajax({
        type: "GET",
        url: $(this).attr('action'),
        data: $(this).serialize(),
        dataType: 'json',
        context: this
-  })
-  .done(function() {
-       $(this).next('div').text('success!').removeClass('error loading');
+  });
+
+  $(this).next('div').addClass('loading').removeClass('error');
+
+  promise.done(function() {
+       $(this).next('div').text('Successfully matched. Please refresh the page to see this change.').removeClass('error loading');
   })
   .fail(function(jqXHR) {
        $(this).next('div')
-           .text($.parseJSON(jqXHR.responseText).error)
+           .text('Error matching: ' + $.parseJSON(jqXHR.responseText).error)
            .addClass('error').removeClass('loading');
   });
-  $(this).next('div').addClass('loading').removeClass('error');
 });
 
 $('form.match-form input').bind('change keyup input propertychange', function(event) {
