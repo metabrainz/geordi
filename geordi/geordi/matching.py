039fb599c14d1a2b672e124952a21d833b58bac4
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-02-28 16:34:32 -0700

    E501 fixes for matching.py

--- geordi/geordi/matching.py
+++ geordi/geordi/matching.py
@@ -39,7 +39,9 @@ def make_match_definition(user, matchtype, mbids, auto=False, ip=False):
     return match
 
 def check_type(mbid):
-    req = urllib2.Request('http://musicbrainz.org/ws/js/entity/{mbid}'.format(mbid=mbid))
+    req = urllib2.Request(
+        'http://musicbrainz.org/ws/js/entity/{mbid}'.format(mbid=mbid)
+    )
     req.add_header('User-Agent', 'Geordi +http://geordi.musicbrainz.org/')
     req.add_header('Accept', 'application/json')
     try:
@@ -49,28 +51,46 @@ def check_type(mbid):
     except urllib2.HTTPError, e:
         return {"error": "failed", "code": e}
 
-def register_match(index, item, itemtype, matchtype, mbids, auto=False, user=None, ip=False):
+def register_match(index, item, itemtype, matchtype, mbids,
+                   auto=False, user=None, ip=False):
     if matchtype != 'unmatch':
         if len(mbids) < 1:
-            response = Response(json.dumps({'code': 400, 'error': 'You must provide at least one MBID for a match.'}), 400, mimetype="application/json")
+            response = Response(json.dumps({
+                'code': 400,
+                'error': 'You must provide at least one MBID for a match.'
+            }), 400, mimetype="application/json")
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
         # Check MBID formatting
         try:
             [uuid.UUID('{{{uuid}}}'.format(uuid=mbid)) for mbid in mbids]
         except ValueError:
-            response = Response(json.dumps({'code': 400, 'error': 'A provided MBID is ill-formed'}), 400, mimetype="application/json")
+            response = Response(json.dumps({
+                'code': 400,
+                'error': 'A provided MBID is ill-formed'
+            }), 400, mimetype="application/json")
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
 
         for mbid in mbids:
             check = check_type(mbid)
             if 'error' in check:
-                response = Response(json.dumps({'code': 400, 'error': 'MBID {} cannot be found in MusicBrainz'.format(mbid)}), 400, mimetype="application/json")
+                response = Response(json.dumps({
+                    'code': 400,
+                    'error':
+                    'MBID {0} cannot be found in MusicBrainz'.format(mbid)
+                }), 400, mimetype="application/json")
                 response.headers.add('Access-Control-Allow-Origin', '*')
                 return response
             elif check['type'] != matchtype:
-                response = Response(json.dumps({'code': 400, 'error': 'Provided match type {provided} doesn\'t match type {mbidtype} of {mbid}'.format(provided=matchtype, mbidtype=check['type'], mbid=mbid)}), 400, mimetype="application/json")
+                response = Response(json.dumps({
+                    'code': 400,
+                    'error': '''Provided match type {provided} doesn\'t match \
+                                type {mbidtype} of {mbid}'''.format(
+                                    provided=matchtype,
+                                    mbidtype=check['type'],
+                                    mbid=mbid)
+                }), 400, mimetype="application/json")
                 response.headers.add('Access-Control-Allow-Origin', '*')
                 return response
             else: continue
@@ -82,7 +102,10 @@ def register_match(index, item, itemtype, matchtype, mbids, auto=False, user=Non
         version = document['_version']
     except ElasticHttpNotFoundError:
         if itemtype == 'item':
-            response = Response(json.dumps({'code': 404, 'error': 'The provided item could not be found.'}), 404, mimetype="application/json")
+            response = Response(json.dumps({
+                'code': 404,
+                'error': 'The provided item could not be found.'
+            }), 404, mimetype="application/json")
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
         else:
@@ -93,12 +116,16 @@ def register_match(index, item, itemtype, matchtype, mbids, auto=False, user=Non
 
     if auto:
         if not user:
-            response = Response(json.dumps({'code': 400, 'error': 'Automatic matches must provide a name.'}), 400, mimetype="application/json")
+            response = Response(json.dumps({
+                'code': 400,
+                'error': 'Automatic matches must provide a name.'
+            }), 400, mimetype="application/json")
             response.headers.add('Access-Control-Allow-Origin', '*')
             return response
         if not ip:
             try:
-                ip = request.environ['HTTP_X_FORWARDED_FOR'].split(',')[-1].strip()
+                forward = request.environ['HTTP_X_FORWARDED_FOR']
+                ip = forward.split(',')[-1].strip()
             except KeyError:
                 ip = request.environ['REMOTE_ADDR']
     else:
@@ -126,10 +153,16 @@ def register_match(index, item, itemtype, matchtype, mbids, auto=False, user=Non
             es.index(index, itemtype, data, id=item, es_version=version)
         else:
             es.index(index, itemtype, data, id=item)
-        response = Response(json.dumps({'code': 200}), 200, mimetype="application/json")
+        response = Response(json.dumps({'code': 200}),
+                            200,
+                            mimetype="application/json")
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
     except:
-        response = Response(json.dumps({'code': 500, 'error': 'An unknown error happened while pushing to elasticsearch.'}), 500, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 500,
+            'error':
+            'An unknown error happened while pushing to elasticsearch.'
+        }), 500, mimetype="application/json")
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
