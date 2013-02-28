c17c9fa3ce3a2457f1386484edcef3726d32311f
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-02-28 16:57:40 -0700

    E501 fixes for views.py

--- geordi/geordi/views.py
+++ geordi/geordi/views.py
@@ -15,12 +15,19 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import division, absolute_import
-from flask import render_template, request, redirect, url_for, flash, Response, g
-from flask.ext.login import login_required, login_user, logout_user, current_user
+from flask import (render_template, request,
+                   redirect, url_for, flash, Response, g)
+from flask.ext.login import (login_required, login_user,
+                             logout_user, current_user)
 from geordi import app, login_manager, User, es
-from geordi.search import do_search, do_search_raw, do_subitem_search, make_filters
+from geordi.search import (do_search, do_search_raw,
+                           do_subitem_search, make_filters)
 from geordi.matching import register_match, check_type
-from geordi.mappings import map_search_data, update_map_by_index, update_linked_by_index, get_link_types_by_index, update_automatic_item_matches_by_index, update_automatic_subitem_matches_by_index, get_mapoptions, get_code_url_by_index
+from geordi.mappings import (map_search_data, update_map_by_index,
+                             update_linked_by_index, get_link_types_by_index,
+                             update_automatic_item_matches_by_index,
+                             update_automatic_subitem_matches_by_index,
+                             get_mapoptions, get_code_url_by_index)
 from geordi.mappings.util import comma_list, comma_only_list
 from geordi.utils import check_data_format
 
@@ -44,7 +51,8 @@ def quote(text):
 @app.before_request
 def before_request():
     g.all_indices = app.config['AVAILABLE_INDICES']
-    g.link_types = dict([(index, get_link_types_by_index(index)) for index in app.config['AVAILABLE_INDICES']])
+    g.link_types = dict([(index, get_link_types_by_index(index))
+                         for index in app.config['AVAILABLE_INDICES']])
     g.json = json
     g.re = re
     g.quote = quote
@@ -66,12 +74,21 @@ def document(itemindex, item):
         subitems = {}
         link_types = get_link_types_by_index(itemindex)
         code_url = get_code_url_by_index(itemindex)
-        for (link_type, links) in data['_source']['_geordi']['links']['links'].iteritems():
+        for (link_type, links)
+        in data['_source']['_geordi']['links']['links'].iteritems():
             if link_type != 'version':
                 for link in links:
-                    subitem = "{}-{}".format(link_type, link[link_types[link_type]['key']])
-                    subitems[subitem] = get_subitem(itemindex, subitem, create=True, seed=copy.deepcopy(link))
-        return render_template(template, item=item, index=itemindex, data=data, mapping=data['_source']['_geordi']['mapping'], mapoptions=mapoptions, subitems=subitems, code_url=code_url)
+                    subitem = "{0}-{1}".format(
+                        link_type, link[link_types[link_type]['key']]
+                    )
+                    subitems[subitem] = get_subitem(itemindex, subitem,
+                                                    create=True,
+                                                    seed=copy.deepcopy(link))
+        return render_template(template,
+                               item=item, index=itemindex, data=data,
+                               mapping=data['_source']['_geordi']['mapping'],
+                               mapoptions=mapoptions,
+                               subitems=subitems, code_url=code_url)
     except ElasticHttpNotFoundError:
         return render_template('notfound.html')
 
@@ -81,7 +98,11 @@ def search():
     params = get_search_params()
     if 'error' in params and params['error'] != 'You must provide a query.':
         flash(params["error"])
-    return render_template('search.html', query=params.get('query'), data=params.get('data'), mapping=params.get('mapping'), start_from=params.get('start_from'), page_size=params.get('page_size'))
+    return render_template('search.html',
+                           query=params.get('query'), data=params.get('data'),
+                           mapping=params.get('mapping'),
+                           start_from=params.get('start_from'),
+                           page_size=params.get('page_size'))
 
 # Internal API urls for matching etc.
 @app.route('/api/search')
@@ -89,9 +110,15 @@ def apisearch():
     "Perform a search, returning JSON"
     params = get_search_params()
     if 'error' in params:
-        response = Response(json.dumps({'code': 400, 'error': params['error']}), 400, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 400, 'error': params['error']
+        }), 400, mimetype="application/json")
     else:
-        response = Response(json.dumps({'code': 200, 'result': params.get('data'), 'mapping': params.get('mapping')}), 200, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 200,
+            'result': params.get('data'),
+            'mapping': params.get('mapping')
+        }), 200, mimetype="application/json")
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -102,14 +129,23 @@ def item(index, item):
         document = resolve_data(index, item)
         if request.args.get('subitems', False):
             link_types = get_link_types_by_index(index)
-            for (link_type, links) in document['_source']['_geordi']['links']['links'].iteritems():
+            for (link_type, links)
+            in document['_source']['_geordi']['links']['links'].iteritems():
                 if link_type != 'version':
                     for link in links:
-                        subitem = "{}-{}".format(link_type, link[link_types[link_type]['key']])
-                        get_subitem(index, subitem, create=True, seed=copy.deepcopy(link))
-        response = Response(json.dumps({'code': 200, 'document': document}), 200, mimetype="application/json")
+                        subitem = "{0}-{1}".format(
+                            link_type, link[link_types[link_type]['key']]
+                        )
+                        get_subitem(index, subitem,
+                                    create=True,
+                                    seed=copy.deepcopy(link))
+        response = Response(json.dumps({
+            'code': 200, 'document': document
+        }), 200, mimetype="application/json")
     except ElasticHttpNotFoundError:
-        response = Response(json.dumps({'code': 404, 'error': 'The provided item could not be found.'}), 404, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 404, 'error': 'The provided item could not be found.'
+        }), 404, mimetype="application/json")
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -120,26 +156,36 @@ def matchitem(index, item):
         auto = False
         user = None
         if request.args.get('unmatch', False):
-            return register_match(index, item, 'item', 'unmatch', [], auto, user)
+            return register_match(index, item, 'item',
+                                  'unmatch', [], auto, user)
     else:
         auto = True
         user = request.args.get('user')
         if user in ['matched by index']:
-            return Response(json.dumps({'code': 400, 'error': 'The name "{}" is reserved.'.format(user)}), 400, mimetype="application/json")
+            return Response(json.dumps({
+                'code': 400,
+                'error': 'The name "{0}" is reserved.'.format(user)
+            }), 400, mimetype="application/json")
     matchtype = request.args.get('type', 'release')
     mbids = request.args.getlist('mbid')
     if not mbids:
         mbids = re.split(',\s*', request.args.get('mbids'))
-    return register_match(index, item, 'item', matchtype, mbids, auto, user)
+    return register_match(index, item, 'item',
+                          matchtype, mbids, auto, user)
 
 @app.route('/api/subitem/<index>/<subitem>')
 def subitem(index, subitem):
     "Get information for a subitem's matching"
     subitem = get_subitem(index, subitem)
     if subitem:
-        response = Response(json.dumps({'code': 200, 'document': subitem}), 200, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 200, 'document': subitem
+        }), 200, mimetype="application/json")
     else:
-        response = Response(json.dumps({'code': 404, 'error': 'The provided item could not be found.'}), 404, mimetype="application/json")
+        response = Response(json.dumps({
+            'code': 404,
+            'error': 'The provided item could not be found.'
+        }), 404, mimetype="application/json")
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
@@ -150,7 +196,8 @@ def matchsubitem(index, subitem):
         auto = False
         user = None
         if request.args.get('unmatch', False):
-            return register_match(index, subitem, 'subitem', 'unmatch', [], auto, user)
+            return register_match(index, subitem, 'subitem',
+                                  'unmatch', [], auto, user)
     else:
         auto = True
         user = request.args.get('user')
@@ -158,20 +205,25 @@ def matchsubitem(index, subitem):
     mbids = request.args.getlist('mbid')
     if not mbids:
         mbids = re.split(',\s*', request.args.get('mbids'))
-    return register_match(index, subitem, 'subitem', matchtype, mbids, auto, user)
+    return register_match(index, subitem, 'subitem',
+                          matchtype, mbids, auto, user)
 
 @app.route('/internal/mbidtype/<mbid>')
 def internal_mbid_type(mbid):
     check = check_type(mbid)
     if 'error' in check:
-        return Response(json.dumps({"error": "failed to fetch"}), e.code, mimetype="application/json")
+        return Response(json.dumps({"error": "failed to fetch"}),
+                        e.code,
+                        mimetype="application/json")
     else:
         return Response(json.dumps(check), 200, mimetype="application/json")
 
 # Login/logout-related views
 @app.route('/login')
 def login():
-    return render_template("login.html", client_id=app.config['OAUTH_CLIENT_ID'], redirect_uri=app.config['OAUTH_REDIRECT_URI'])
+    return render_template("login.html",
+                           client_id=app.config['OAUTH_CLIENT_ID'],
+                           redirect_uri=app.config['OAUTH_REDIRECT_URI'])
 
 @app.route('/internal/oauth')
 def oauth_callback():
@@ -192,7 +244,9 @@ def oauth_callback():
             flash('Incorrect username, please try again.')
     else:
         flash('There was an error: ' + error)
-    return render_template("login.html", client_id=app.config['OAUTH_CLIENT_ID'], redirect_uri=app.config['OAUTH_REDIRECT_URI'])
+    return render_template("login.html",
+                           client_id=app.config['OAUTH_CLIENT_ID'],
+                           redirect_uri=app.config['OAUTH_REDIRECT_URI'])
 
 @app.route("/logout")
 @login_required
@@ -207,7 +261,9 @@ def resolve_data(index, item):
     data = es.get(index, 'item', item)
     linked_update = update_linked_by_index(index, item, data['_source'])
     map_update = update_map_by_index(index, item, data['_source'])
-    match_update = update_automatic_item_matches_by_index(index, item, data['_source'])
+    match_update = update_automatic_item_matches_by_index(index,
+                                                          item,
+                                                          data['_source'])
     if linked_update or map_update or match_update:
         data = es.get(index, 'item', item)
     update_automatic_subitem_matches_by_index(index, item, data['_source'])
@@ -229,7 +285,9 @@ def get_search_params():
     if len(itemtypes) == 0:
         itemtypes = ['item']
 
-    filters = make_filters(human=request.args.get('human', False), auto=request.args.get('auto', False), un=request.args.get('un', False))
+    filters = make_filters(human=request.args.get('human', False),
+                           auto=request.args.get('auto', False),
+                           un=request.args.get('un', False))
 
     if search_type == 'raw':
         json_query = json.loads(query)
@@ -239,12 +297,17 @@ def get_search_params():
             except:
                 pass
         try:
-            data = do_search_raw(json_query, indices, start_from=request.args.get('from', None), filters=filters, doc_type=itemtypes, size=size)
+            data = do_search_raw(json_query, indices,
+                                 start_from=request.args.get('from', None),
+                                 filters=filters, doc_type=itemtypes,
+                                 size=size)
         except ValueError:
             return {'error': "Malformed or missing JSON."}
     elif search_type == 'query':
         if query:
-            data = do_search(query, indices, start_from=request.args.get('from', None), filters=filters, doc_type=itemtypes, size=size)
+            data = do_search(query, indices,
+                             start_from=request.args.get('from', None),
+                             filters=filters, doc_type=itemtypes, size=size)
         else:
             return {'error': 'You must provide a query.'}
     elif search_type == 'sub':
@@ -252,17 +315,24 @@ def get_search_params():
         subtype = request.args.get('subitem_type')
         if subtype not in get_link_types_by_index(index).keys():
             return {'error': 'Invalid subitem type for index {}'.format(index)}
-        data = do_subitem_search(query, index, subtype, start_from=request.args.get('from', None), filters=filters, size=size)
+        data = do_subitem_search(query, index, subtype,
+                                 start_from=request.args.get('from', None),
+                                 filters=filters, size=size)
     else:
         return {'error': 'Search type {} unimplemented.'.format(search_type)}
 
     mapping = map_search_data(data)
 
-    return {"start_from": start_from, "query": query, "mapping": mapping, "data": data, "page_size": size}
+    return {"start_from": start_from,
+            "query": query,
+            "mapping": mapping,
+            "data": data,
+            "page_size": size}
 
 def flatten(l):
     for el in l:
-        if isinstance(el, collections.Iterable) and not isinstance(el, basestring):
+        if isinstance(el,
+                      collections.Iterable) and not isinstance(el, basestring):
             for sub in flatten(el):
                 yield sub
         else:
@@ -284,13 +354,17 @@ def get_subitem(index, subitem, create=False, seed={}):
                 elif key in data:
                     changed = True
                     try:
-                        if unicode(seed[key]) not in [unicode(i) for i in data[key]]:
+                        if (
+                            unicode(seed[key]) not in
+                            [unicode(i) for i in data[key]]
+                        ):
                             data[key].append(seed[key])
                         changed = True
                     except (AttributeError, TypeError):
                         data[key] = list(flatten([data[key], seed[key]]))
                         changed = True
-                    if isinstance(data[key], collections.Iterable) and not isinstance(data[key], basestring):
+                    if ((isinstance(data[key], collections.Iterable) and
+                         not isinstance(data[key], basestring))):
                         data[key] = list(set(flatten(data[key])))
             if changed:
                 es.index(index, 'subitem', data, id=subitem)
@@ -307,13 +381,15 @@ def check_mb_account(username, auth_code):
     data = urllib.urlencode({'grant_type': 'authorization_code',
                              'code': auth_code,
                              'client_id': app.config['OAUTH_CLIENT_ID'],
-                             'client_secret': app.config['OAUTH_CLIENT_SECRET'],
+                             'client_secret':
+                             app.config['OAUTH_CLIENT_SECRET'],
                              'redirect_uri': app.config['OAUTH_REDIRECT_URI']})
     json_data = json.load(urllib2.urlopen(url, data))
 
     url = 'https://beta.musicbrainz.org/ws/1/user?name=' + username
     opener = urllib2.build_opener()
-    opener.addheaders = [('Authorization', 'Bearer ' + json_data['access_token'])]
+    opener.addheaders = [('Authorization',
+                          'Bearer ' + json_data['access_token'])]
     try:
         opener.open(url, timeout=5)
     except StandardError:
