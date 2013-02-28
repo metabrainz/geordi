0d4a00dcfd035d8e6d5fd6080a488f18e95526e8
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-02-28 09:55:14 -0700

    By using app.before_first_request, *really* only set up logging when debug is off

--- geordi/geordi/__init__.py
+++ geordi/geordi/__init__.py
@@ -23,15 +23,17 @@ from pyelasticsearch import ElasticSearch
 
 app = Flask(__name__)
 app.config.from_object(__name__)
-if not app.debug:
-    import logging
-    from logging.handlers import RotatingFileHandler
-    error_filehandler = RotatingFileHandler('/var/log/geordi/errors.log', maxBytes=1024 * 1024 * 10, backupCount=10, encoding='utf_8')
-    error_filehandler.setLevel(logging.ERROR)
-    warning_filehandler = RotatingFileHandler('/var/log/geordi/warnings.log', maxBytes=1024 * 1024 * 10, backupCount=10, encoding='utf_8')
-    warning_filehandler.setLevel(logging.WARNING)
-    app.logger.addHandler(error_filehandler)
-    app.logger.addHandler(warning_filehandler)
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        import logging
+        from logging.handlers import RotatingFileHandler
+        error_filehandler = RotatingFileHandler('/var/log/geordi/errors.log', maxBytes=1024 * 1024 * 10, backupCount=10, encoding='utf_8')
+        error_filehandler.setLevel(logging.ERROR)
+        warning_filehandler = RotatingFileHandler('/var/log/geordi/warnings.log', maxBytes=1024 * 1024 * 10, backupCount=10, encoding='utf_8')
+        warning_filehandler.setLevel(logging.WARNING)
+        app.logger.addHandler(error_filehandler)
+        app.logger.addHandler(warning_filehandler)
 
 class User(UserMixin):
     def __init__(self, id):
