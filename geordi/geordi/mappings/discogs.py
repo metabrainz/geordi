733547668bdc26367ae24fd3aa120fe99d7e1051
nikki <nikki@shoelace>
2013-04-05 03:32:22 +0200

    Increase version number

--- geordi/geordi/mappings/discogs.py
+++ geordi/geordi/mappings/discogs.py
@@ -40,7 +40,7 @@ class discogs(MappingBase):
 
     def map(self, data):
         target = base_mapping('release')
-        target['version'] = 2
+        target['version'] = 3
         release = target['release']
 
         try:
