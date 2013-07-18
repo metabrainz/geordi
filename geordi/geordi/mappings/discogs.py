ebe19e1a2ffca3650f4067dfc5ebf5b0cb2e4d73
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-07-18 10:50:07 -0700

    GEORDI-16: hopefully match artists and labels automatically too

--- geordi/geordi/mappings/discogs.py
+++ geordi/geordi/mappings/discogs.py
@@ -348,6 +348,16 @@ class discogs(MappingBase):
                 return {'release-group': mbids}
             except:
                 return {'unmatch': []}
+        elif discogs_type in ['artist', 'label'] and data.get('name', False):
+            name = data.get('name')[0]
+            try:
+                url_data = musicbrainzngs.browse_urls(
+                    resource='http://www.discogs.com/%s/%s' % (discogs_type, name),
+                    includes=['%s-rels' % discogs_type])
+                mbids = [entity[discogs_type]['id'] for entity in url_data['url']['%s-relation-list' % discogs_type]]
+                return {discogs_type: mbids}
+            except:
+                return {'unmatch': []}
         else:
             return {}
 
