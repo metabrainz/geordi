adfc848b0265bbe7b8aa4bb2828201e7c76d4335
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-07-18 16:23:09 -0700

    further skeleton work on updater script

--- tools/discogs-update/updater.py
+++ tools/discogs-update/updater.py
@@ -17,13 +17,38 @@ es = ElasticSearch(ELASTICSEARCH_ENDPOINT)
 PUBLIC_ENDPOINT = 'http://geordi.musicbrainz.org'
 def initialize():
     # for match in geordi, update
-    # for match in MB, update (check if successful; store match if so)
-    pass
+    stored_data = []
+    existing_matches = []
+    for match in existing_matches:
+        data = update_match(match['itemtype'], match['id'])
+        if data[2] is not None:
+            stored_data.append(data)
+
+    store_data(stored_data)
+
+    # for match in MB, update (store most recent match after update)
+    update()
 
 def update():
     # get existing matches
+    existing = get_stored_data()
     # for match in MB, skip and remove from existing match list if exactly the same, otherwise update (store whichever match is most recent after update)
+    stored_data = []
+    mb_matches = []
+    for match in mb_matches:
+        # resolve name here, if appropriate
+        data = update_match(match['itemtype'], match['id'])
+        if data[2] is not None:
+            stored_data.append(data)
+
     # for remaining matches in existing, update and store most recent match afterwards [should be unmatches]
+
+    store_data(stored_data)
+
+def store_data(stored_data):
+    pass
+
+def get_stored_data():
     pass
 
 def update_match(itemtype, identifier):
@@ -40,7 +65,11 @@ def update_match(itemtype, identifier):
     sleep(1)
     # then a fetch
     json_data = json.load(urllib2.urlopen(url))
-    return json_data['document']['_source']['_geordi']['matchings']['current_matching'].get('mbid', None)
+    mbids = json_data['document']['_source']['_geordi']['matchings']['current_matching'].get('mbid', None)
+    if itemtype not in ['release', 'master']:
+        return (itemtype, identifier, mbids, json_data['document']['_source']['name'])
+    else:
+        return (itemtype, identifier, mbids)
 
 commands = {'initialize': initialize, 'update': update}
 if __name__ == '__main__':
