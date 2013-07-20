c159a39a819f66459dd11e6d73909f419ed0e935
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2013-07-20 06:15:03 -0700

    Basic functionality! Other than the initialization from elasticsearch matches.

--- tools/discogs-update/updater.py
+++ tools/discogs-update/updater.py
@@ -5,6 +5,7 @@ from pyelasticsearch import ElasticSearch, ElasticHttpNotFoundError
 import sys
 import os
 import json
+import urllib
 import urllib2
 
 from time import sleep
@@ -15,6 +16,8 @@ from config import ELASTICSEARCH_ENDPOINT, PUBLIC_ENDPOINT
 es = ElasticSearch(ELASTICSEARCH_ENDPOINT)
 
 DATA_FILE = './data.json'
+MB_FILE = './mb-data.tsv'
+
 PUBLIC_ENDPOINT = 'http://geordi.musicbrainz.org'
 def initialize():
     # for match in geordi, update
@@ -42,21 +45,30 @@ def update():
         else:
             key = match['itemtype'] + ':' + match['id']
 
+        print('Doing ' + key)
         if existing.get(key, False):
             if ','.join(match['mbids']) == ','.join(existing[key][2]):
+                print('Already done.')
                 stored_data.append(existing[key])
             else:
+                print('MBIDs differ, updating.')
                 data = update_match(existing[key][0], existing[key][1])
                 if data[2] is not None:
                     stored_data.append(data)
             del existing[key]
         else:
+            print('No record, resolving name etc.')
             # resolve name here, if appropriate
-            if match['itemtype'] not in ['release', 'master']:
-                match['id'] = get_id(match['itemtype'], match['name'])
-            data = update_match(match['itemtype'], match['id'])
-            if data[2] is not None:
-                stored_data.append(data)
+            try:
+                if match['itemtype'] not in ['release', 'master']:
+                    match['id'] = get_id(match['itemtype'], match['name'])
+                print('Found name ' + match['name'] + ' maps to id ' + match['id'])
+                data = update_match(match['itemtype'], match['id'])
+                if data[2] is not None:
+                    stored_data.append(data)
+            except:
+                print('Name not found in geordi. Skipping.')
+                continue
 
     # for remaining matches in existing, update and store most recent match afterwards [should be unmatches]
     for (key, match) in existing.iteritems():
@@ -67,10 +79,38 @@ def update():
     store_data(stored_data)
 
 def get_id(itemtype, name):
-    raise Exception('unimplemented')
+    url = PUBLIC_ENDPOINT + '/api/search'
+    url_data = urllib.urlencode({'type': 'query',
+            'query': '_id:' + itemtype + '-* and name:"' + name + '"',
+            'index': 'discogs',
+            'itemtype': 'subitem'})
+    data = json.load(urllib2.urlopen(url + '?' + url_data))
+    try:
+        first_result = data['result']['hits']['hits'][0]
+        geordi_name = first_result['_source']['name']
+        entity_id = first_result['_id'].split('-')[1]
+    except:
+        raise Exception('Failed to get first result properly.')
+
+    if name in geordi_name:
+        return entity_id
+    else:
+        raise Exception('Name doesn\'t match')
 
 def get_from_mb():
-    return []
+    mbdata = []
+    fh = open(MB_FILE, 'r')
+    for line in fh.readlines():
+        # Format is <type>\t<ID, parsed from URL>\t<MBIDs, comma-separated>
+        (itemtype, identifier, mbid_string) = line[:-1].split('\t')
+        mbids = mbid_string.split(',')
+
+        if itemtype not in ['release', 'master']:
+            mbdata.append({'itemtype': itemtype, 'name': identifier, 'mbids': mbids})
+        else:
+            mbdata.append({'itemtype': itemtype, 'id': identifier, 'mbids': mbids})
+    fh.close()
+    return mbdata
 
 def get_from_elasticsearch():
     return []
@@ -79,7 +119,7 @@ def store_data(stored_data):
     store_dict = {}
     for entry in stored_data:
         if entry[0] not in ['release', 'master']:
-            key = entry[0] + ':' + entry[3]
+            key = entry[0] + ':' + entry[3][0]
         else:
             key = entry[0] + ':' + entry[1]
         store_dict[key] = entry
@@ -88,10 +128,13 @@ def store_data(stored_data):
     fh.close()
 
 def get_stored_data():
-    fh = open(DATA_FILE, 'r')
-    data = json.load(fh)
-    fh.close()
-    return data
+    try:
+        fh = open(DATA_FILE, 'r')
+        data = json.load(fh)
+        fh.close()
+        return data
+    except:
+        return {}
 
 def update_match(itemtype, identifier):
     # We're assuming you're passing a real ID number, not a name.
@@ -115,6 +158,5 @@ def update_match(itemtype, identifier):
 
 commands = {'initialize': initialize, 'update': update}
 if __name__ == '__main__':
-    print(update_match('master', '506527'))
-    #command = sys.argv[1]
-    #commands[command]()
+    command = sys.argv[1]
+    commands[command]()
