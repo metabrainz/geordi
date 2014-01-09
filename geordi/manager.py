3e3ef04e1cb527528b354f43ff239eef9c78a47c
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-09 02:36:03 -0700

    basic manager.py and stub application

---
+++ geordi/manager.py
@@ -0,0 +1,6 @@
+from flask.ext.script import Manager
+from geordi import app
+
+manager = Manager(app)
+if __name__ == "__main__":
+    manager.run()
--- geordi/run.py
+++
@@ -1,2 +0,0 @@
-from geordi import app
-app.run(debug=True)
