a913668845fcb01dbefa3d4d02924067ad51212f
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-09 04:05:32 -0700

    add login manager and settings handling

--- geordi/geordi/__init__.py
+++ geordi/geordi/__init__.py
@@ -1,7 +1,27 @@
+from __future__ import division, absolute_import
 from flask import Flask
+from flask.ext.login import LoginManager, UserMixin
 from geordi.frontend import frontend
+import geordi.settings
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+login_manager = LoginManager()
+login_manager.login_view = "frontend.login"
+
+@login_manager.user_loader
+def load_user(username):
+    return User(username)
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('geordi.settings')
+    app.config.from_pyfile('settings.cfg', silent=True)
+
+    login_manager.init_app(app)
+
     app.register_blueprint(frontend)
+
     return app
--- geordi/geordi/frontend/__init__.py
+++ geordi/geordi/frontend/__init__.py
@@ -1,7 +1,13 @@
 from flask import Blueprint
+from flask.ext.login import login_required
 
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route('/')
+@login_required
 def hello_world():
     return 'Hello World!'
+
+@frontend.route('/login')
+def login():
+    return 'login? bah'
