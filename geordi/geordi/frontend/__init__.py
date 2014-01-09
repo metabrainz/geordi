870b9d1ab63775cbd76a7136603a0e8d40d02fec
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-09 09:04:00 -0700

    basic layout template

--- geordi/geordi/frontend/__init__.py
+++ geordi/geordi/frontend/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask.ext.login import login_required
 from geordi.db import get_db
 
@@ -8,8 +8,8 @@ frontend = Blueprint('frontend', __name__)
 def hello_world():
     with get_db().cursor() as curs:
         curs.execute("SELECT 'Hello World!';")
-        (v) = curs.fetchone()
-    return v
+        (v,) = curs.fetchone()
+    return render_template('hello.html', hello=v)
 
 @frontend.route('/login')
 def login():
