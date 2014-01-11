5b14cd1753c913e2070dda5535b636ceabdeab1d
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-10 23:29:18 -0700

    Expand data management and data API generally further.

--- geordi/geordi/data/manage.py
+++ geordi/geordi/data/manage.py
@@ -1,4 +1,5 @@
 import geordi.data
+import json
 from flask.ext.script import Manager
 
 data_manager = Manager(usage="Manipulate and query the elasticsearch and postgresql databases.")
@@ -17,6 +18,10 @@ def show_data_item(data_id):
 def add_data_item(data_id, data_type, data):
     '''Add or update a data item given a data ID, a type, and the data to use.'''
     print geordi.data.add_data_item(data_id, data_type, data)
+
+@data_manager.command
+def delete_data_item(data_id):
+    print geordi.data.delete_data_item(data_id)
 
 @data_manager.command
 def add_match(item_id, editor, mbid_type, mbid):
--- geordi/geordi/data/utils.py
+++ geordi/geordi/data/utils.py
@@ -4,9 +4,12 @@ import json
 def get_item(item_id):
     '''Fetch and return an item's data.'''
     with get_db().cursor() as curs:
-        curs.execute('''SELECT '{"blah": "foo", "baz": {"quux": "lol"}}';''')
-        data = [json.loads(d[0]) for d in curs.fetchall()]
-    return {'id': item_id, 'data': data}
+        curs.execute('SELECT data FROM item_data WHERE item = %s;', (item_id,))
+        if curs.rowcount > 0:
+            data = [json.loads(d[0]) for d in curs.fetchall()]
+            return {'id': item_id, 'data': data}
+        else:
+            return None
 
 def get_renderable(item_id):
     '''Fetch and return an item's data including a pretty-printed version of the data items.'''
@@ -28,31 +31,38 @@ def data_to_item(data_id):
 def add_data_item(data_id, data_type, data):
     '''Add or update a data item given an ID, type, and data. Create a new item, for additions.'''
     item_id = data_to_item(data_id)
-    if item_id is None:
-        item_id = _create_item(data_type)
-    _register_data_item(data_id, item_id, data)
-    _map_item(item_id)
+    with get_db() as conn:
+        if item_id is None:
+            item_id = _create_item(data_type, conn)
+        _register_data_item(item_id, data_id, data, conn)
+        _map_item(item_id, conn)
+
+def delete_data_item(data_id):
+    with get_db() as conn, conn.cursor() as curs:
+        curs.execute('DELETE FROM item_data WHERE id = %s RETURNING item', (data_id,))
+        item = curs.fetchone()[0]
+        curs.execute('DELETE FROM item WHERE id = %s AND NOT EXISTS (SELECT true FROM item_data WHERE item = item.id)', (item,))
 
 def match_item(item_id, editor, mbid_type, mbid):
     '''Register a match with an item ID, editor, entity type, and MBID.'''
     pass
 
-def _create_item(data_type):
-    with get_db() as conn, conn.cursor() as curs:
+def _create_item(data_type, conn):
+    with conn.cursor() as curs:
         curs.execute('INSERT INTO item (type) VALUES (%s) RETURNING id', (data_type,))
         if curs.rowcount == 1:
             return curs.fetchone()[0]
         else:
             raise Exception('No row created, or more than one.')
 
-def _map_item(item_id):
+def _map_item(item_id, conn):
     # fetch data
     # generate map
     # insert/propagate to relevant databases
     pass
 
-def _register_data_item(item_id, data_id, data):
-    with get_db() as conn, conn.cursor() as curs:
+def _register_data_item(item_id, data_id, data, conn):
+    with conn.cursor() as curs:
         curs.execute('INSERT INTO item_data (id, item, data) VALUES (%s, %s, %s) RETURNING (item, id)', (data_id, item_id, data))
         if curs.rowcount == 1:
             return curs.fetchone()
