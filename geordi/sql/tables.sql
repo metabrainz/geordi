1ac00bdff8f9e1f28b6adb048e221283648cb509
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-10 19:32:29 -0700

    update sql definition slightly

--- geordi/sql/tables.sql
+++ geordi/sql/tables.sql
@@ -3,13 +3,13 @@ BEGIN;
 -- Schema 'geordi' should already exist.
 CREATE TABLE geordi.csrf (ip text NOT NULL, csrf text NOT NULL);
 
-CREATE TABLE geordi.user (name text PRIMARY KEY, tz text, internal boolean not null default false);
+CREATE TABLE geordi.editor (name text PRIMARY KEY, tz text, internal boolean not null default false);
 
-CREATE TABLE geordi.item (id integer PRIMARY KEY, type text, map text, dirty boolean not null default false);
-CREATE TABLE geordi.item_data (data_id text PRIMARY KEY, item_id integer PRIMARY KEY REFERENCES item(id));
+CREATE TABLE geordi.item (id serial PRIMARY KEY, type text, map text, dirty boolean not null default false);
+CREATE TABLE geordi.item_data (data_id text, item_id integer REFERENCES item(id), PRIMARY KEY (data_id, item_id));
 CREATE TABLE geordi.item_redirect (old integer PRIMARY KEY, new integer REFERENCES item(id));
 CREATE TABLE geordi.item_link (item integer not null REFERENCES item(id), linked integer not null REFERENCES item(id), type text not null);
 
-CREATE TABLE geordi.raw_match (item integer NOT NULL REFERENCES item(id), user text NOT NULL REFERENCES user(name), type text not null, mbid text not null, timestamp TIMESTAMP WITH TIME ZONE not null, superseded boolean not null default false);
+CREATE TABLE geordi.raw_match (item integer NOT NULL REFERENCES item(id), editor text NOT NULL REFERENCES editor(name), type text not null, mbid text not null, timestamp TIMESTAMP WITH TIME ZONE not null, superseded boolean not null default false);
 
 COMMIT;
