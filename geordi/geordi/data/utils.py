51b34cf75fe7565fd5acc1a53e1ed5f6f2846167
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-10 19:40:40 -0700

    flesh out a few more data methods, fix imports

--- geordi/geordi/data/utils.py
+++ geordi/geordi/data/utils.py
@@ -13,19 +13,37 @@ def get_renderable(item_id):
     return item
 
 def add_data(data_id, data_type, data):
-    pass
+    item_id = data_to_item(data_id)
+    if item_id is None:
+        item_id = create_item(data_type)
+        register_data(data_id, item_id)
+    # insert actual data to elasticsearch
+    map_item(item_id)
 
 def data_to_item(data_id):
-    pass
+    item_id = None
+    with get_db().cursor() as curs:
+        curs.execute('SELECT item_id FROM item_data WHERE data_id = %s', (data_id,))
+        if curs.rowcount == 1:
+            (item_id,) = curs.fetchone()
+        elif curs.rowcount > 1:
+            # error state
+            raise Exception('More than one item found, that can\'t be right')
+    return item_id
 
 def map_item(item_id):
     pass
 
 def create_item(data_type):
-    pass
+    with get_db().cursor() as curs:
+        curs.execute('INSERT INTO item (type) VALUES (%s) RETURNING id', (data_type,))
+        if curs.rowcount == 1:
+            return curs.fetchone()[0]
+        else:
+            raise Exception('No row created, or more than one.')
 
 def register_data(data_id, item_id):
     pass
 
-def match_item():
+def match_item(item_id, editor, mbid_type, mbid):
     pass
--- geordi/geordi/frontend/__init__.py
+++ geordi/geordi/frontend/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
-from geordi.db import get_db
-import geordi.data as data
+from ..db import get_db
+import ..data
 import json
 
 frontend = Blueprint('frontend', __name__)
