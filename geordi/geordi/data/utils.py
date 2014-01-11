6282797f8938957b3fc97f9a23f26ebf46c165c4
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-10 23:14:49 -0700

    document things, add data item data to postgres database

--- geordi/geordi/data/utils.py
+++ geordi/geordi/data/utils.py
@@ -2,20 +2,23 @@ from ..db import get_db
 import json
 
 def get_item(item_id):
+    '''Fetch and return an item's data.'''
     with get_db().cursor() as curs:
         curs.execute('''SELECT '{"blah": "foo", "baz": {"quux": "lol"}}';''')
         data = [json.loads(d[0]) for d in curs.fetchall()]
     return {'id': item_id, 'data': data}
 
 def get_renderable(item_id):
+    '''Fetch and return an item's data including a pretty-printed version of the data items.'''
     item = get_item(item_id)
     item['data_formatted'] = [json.dumps(i, indent=4) for i in item.get('data', [])]
     return item
 
 def data_to_item(data_id):
+    '''Resolve a data ID to its associated item ID, if it has one (it should!)'''
     item_id = None
     with get_db().cursor() as curs:
-        curs.execute('SELECT item FROM item_data WHERE data_id = %s', (data_id,))
+        curs.execute('SELECT item FROM item_data WHERE id = %s', (data_id,))
         if curs.rowcount == 1:
             (item_id,) = curs.fetchone()
         elif curs.rowcount > 1:
@@ -23,14 +26,15 @@ def data_to_item(data_id):
     return item_id
 
 def add_data_item(data_id, data_type, data):
+    '''Add or update a data item given an ID, type, and data. Create a new item, for additions.'''
     item_id = data_to_item(data_id)
     if item_id is None:
         item_id = _create_item(data_type)
-        _register_data_item(data_id, item_id)
-    # insert actual data as appropriate
+    _register_data_item(data_id, item_id, data)
     _map_item(item_id)
 
 def match_item(item_id, editor, mbid_type, mbid):
+    '''Register a match with an item ID, editor, entity type, and MBID.'''
     pass
 
 def _create_item(data_type):
@@ -42,11 +46,14 @@ def _create_item(data_type):
             raise Exception('No row created, or more than one.')
 
 def _map_item(item_id):
+    # fetch data
+    # generate map
+    # insert/propagate to relevant databases
     pass
 
-def _register_data_item(item_id, data_id):
+def _register_data_item(item_id, data_id, data):
     with get_db() as conn, conn.cursor() as curs:
-        curs.execute('INSERT INTO item_data (item, data_id) VALUES (%s, %s) RETURNING (item, data_id)', (item_id, data_id))
+        curs.execute('INSERT INTO item_data (id, item, data) VALUES (%s, %s, %s) RETURNING (item, id)', (data_id, item_id, data))
         if curs.rowcount == 1:
             return curs.fetchone()
         else:
--- geordi/sql/tables.sql
+++ geordi/sql/tables.sql
@@ -5,8 +5,8 @@ CREATE TABLE geordi.csrf (ip text NOT NULL, csrf text NOT NULL);
 
 CREATE TABLE geordi.editor (name text PRIMARY KEY, tz text, internal boolean not null default false);
 
-CREATE TABLE geordi.item (id serial PRIMARY KEY, type text, map text, dirty boolean not null default false);
-CREATE TABLE geordi.item_data (data_id text PRIMARY KEY, item integer not null REFERENCES item(id));
+CREATE TABLE geordi.item (id serial PRIMARY KEY, type text);
+CREATE TABLE geordi.item_data (id text PRIMARY KEY, item integer not null REFERENCES item(id), data text);
 CREATE TABLE geordi.item_redirect (old integer PRIMARY KEY, new integer REFERENCES item(id));
 CREATE TABLE geordi.item_link (item integer not null REFERENCES item(id), linked integer not null REFERENCES item(id), type text not null);
 
