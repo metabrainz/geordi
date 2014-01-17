cf76061eb4578de0a959916e0d5ab360c4ca7e28
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-01-16 21:54:39 -0700

    sketch out geordi.data.mapping API

--- geordi/geordi/data/mapping/__init__.py
+++ geordi/geordi/data/mapping/__init__.py
@@ -1,1 +1,19 @@
 from .indexes import *
+
+def map_item(item):
+    '''Map an item, returning the final mapped data, links, blank nodes, etc.'''
+    # for data item
+    #   generate map data
+    # merge
+    # flatten
+    pass
+
+def map_data_item(data_id, data):
+    '''Map a data item, returning the appropriate internal representation for merging/flattening'''
+    # determine index and item type to use
+    # get rules to use
+    # for each rule:
+    #  if in document, and condition matches:
+    #    run transformations
+    #    calculate destination, put in structure by node, value, and ordering
+    pass
