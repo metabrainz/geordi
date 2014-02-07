0494ae84350704bba38703eaee9f9b62d0e1a6e9
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-02-06 18:31:18 -0700

    use alternate test_index album mapping using a transform returning a dict

--- geordi/geordi/data/mapping/indexes/test_index.py
+++ geordi/geordi/data/mapping/indexes/test_index.py
@@ -8,13 +8,15 @@ test_index = {
               Rule(['tracks', ('t_index', lambda a: True), 'duration'],
                    lambda *args, **kwargs: ['release','tracks', kwargs.get('t_index'), 'length'],
                    transform=lambda a, *args, **kwargs: int(a) * 1000),
-              Rule(['artists', ('a_index', lambda a: True), 'name'],
-                   lambda *args, **kwargs: ['release', 'artists', kwargs.get('a_index'), 'name']),
-              Rule(['artists', ('a_index', lambda a: True), 'credit'],
-                   lambda *args, **kwargs: ['release', 'artists', kwargs.get('a_index'), 'credit']),
               Rule(['artists', ('a_index', lambda a: True)],
                    lambda *args, **kwargs: ['release', 'artists', kwargs.get('a_index')],
-                   link=lambda value, data, *args, **kwargs: 'test_index/artist/%s' % value.get('id')),
+                   transform=lambda value, *args, **kwargs: {'name': value['name'], 'credit': value.get('credit', value['name'])},
+                   link=lambda value, data, *args, **kwargs: 'test_index/artist/%s' % value.get('id'))
+              # alternately, remove 'transform' above and use:
+              #Rule(['artists', ('a_index', lambda a: True), 'name'],
+              #     lambda *args, **kwargs: ['release', 'artists', kwargs.get('a_index'), 'name']),
+              #Rule(['artists', ('a_index', lambda a: True), 'credit'],
+              #     lambda *args, **kwargs: ['release', 'artists', kwargs.get('a_index'), 'credit']),
              ],
     'artist': [
                Rule(['name'],
