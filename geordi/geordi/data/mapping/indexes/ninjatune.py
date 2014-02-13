dc9c3ee85bef8bc87ba212fbd9ad071476574208
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-02-12 23:18:26 -0700

    Start of ninjatune mapping.

---
+++ geordi/geordi/data/mapping/indexes/ninjatune.py
@@ -0,0 +1,5 @@
+from ..rule import Rule
+ninjatune = {
+    'release': [Rule(['PRODUCT TITLE', ('index', True)], ['release', 'title']),
+                Rule(['Product Title', ('index', True)], ['release', 'title'])]
+}
