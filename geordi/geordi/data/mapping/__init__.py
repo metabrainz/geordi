1514eb56cb406169eb79384309457776bd7a6189
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-03-12 00:30:21 -0700

    properly flatten considering links, though this needs more testing

--- geordi/geordi/data/mapping/__init__.py
+++ geordi/geordi/data/mapping/__init__.py
@@ -10,34 +10,88 @@ import json
 
 logger = logging.getLogger('geordi.data.mapping')
 
-def _flatten(data):
-    logger.debug('_flatten %r', data)
-    if isinstance(data, collections.Mapping):
-        return dict([(k, _flatten(data[k])) for k in data.keys()])
-    elif isinstance(data, list):
-        if len(data) > 0 and isinstance(data[0], tuple):
-            # note that this throws out link info, which should already have been dealt with
-            return [_flatten(d[0]) for d in sorted(data,key=lambda x: x[1])]
+def _filter_links(links, path):
+    return [link for link in links if (link[0] == path[0] and link[1][:len(path)-1] == path)]
+
+def _replace_ident(link, path, identifier, replace):
+    relevant_entry = link[1][len(path)-1]
+    if isinstance(relevant_entry, tuple) and relevant_entry[1] == identifier:
+        return tuple([link[0], link[1][:len(path)-1] + [replace] + link[1][len(path)+1:]] + list(link[2:]))
+
+def _partition_links(links, path, known_keys):
+    logger.debug('_partition_links %r %r %r', links, path, known_keys)
+    part = dict([(k, []) for k in known_keys])
+    if part.get(None) == []:
+        raise Exception('None is among partition keys, abort')
+    part[None] = []
+    for l in links:
+        cmp_path = [l[0]] + l[1]
+        if cmp_path[:len(path)] == path and cmp_path[len(path)] in known_keys:
+            part[cmp_path[len(path)]].append(l)
         else:
-            return [_flatten(d) for d in data]
+            part[None].append(l)
+    logger.debug('partitioned links: %r', part)
+    return part
+
+def _tuple_to_link_tuple(t):
+    if len(t) >= 3:
+        return tuple(t[1:3])
     else:
-        return data
+        return (t[1], t[1])
+
+def _flatten_with_links(data, links, path=[]):
+    logger.debug('_flatten_with_links %r %r %r', data, links, path)
+    if isinstance(data, collections.Mapping):
+        ret = {}
+        ret_links = []
+        partitioned_links = _partition_links(links, path, data.keys())
+        for k in data.keys():
+            new_path = path + [k]
+            (flat, l) = _flatten_with_links(data[k], partitioned_links[k], new_path)
+            ret[k] = flat
+            ret_links.extend(l)
+        ret_links.extend(partitioned_links[None])
+        return (ret, ret_links)
+    elif isinstance(data, list):
+        ret = []
+        ret_links = []
+        if len(data) > 0 and isinstance(data[0], tuple):
+            partitioned_links = _partition_links(links, path, [_tuple_to_link_tuple(i) for i in data])
+            data_sorted = sorted(data, key=lambda x: x[1])
+            for i in range(len(data_sorted)):
+                k = _tuple_to_link_tuple(data_sorted[i])
+                new_links = [_replace_ident(link, path, k[1], i) for link in partitioned_links[k]]
+                new_path = path + [i]
+                (flat, l) = _flatten_with_links(data_sorted[i][0], new_links, new_path)
+                ret.append(flat)
+                ret_links.extend(l)
+        else:
+            partitioned_links = _partition_links(links, path, list(range(len(data))))
+            for k in range(len(data)):
+                new_path = path + [k]
+                (flat, l) = _flatten_with_links(data[k], partitioned_links[k], new_path)
+                ret.append(flat)
+                ret_links.extend(l)
+        ret_links.extend(partitioned_links[None])
+        return (ret, ret_links)
+    else:
+        return (data, links)
 
 def map_item(item):
     '''Map an item, returning the final mapped data, links, blank nodes, etc.'''
     logger.debug('map_item %r', item)
     mapped_data = {}
     links = []
+    d = None
     if len(item['data'].keys()) <= 1:
         for (data_id, data) in item['data'].iteritems():
-            (mapped_data, links) = map_data_item(data_id, data)
+            (mapped_data, links) = _flatten_with_links(*map_data_item(data_id, data))
             mapped_data[None] = mapped_data[data_id]
             del mapped_data[data_id]
     else:
         raise Exception('unimplemented')
     # merge XXX: implement along with merging of items
-    # XXX: links for ordering-tuple entries
-    return (_flatten(mapped_data), links)
+    return (mapped_data, links)
 
 def map_data_item(data_id, data):
     '''Map a data item, returning the appropriate internal representation for merging/flattening'''
