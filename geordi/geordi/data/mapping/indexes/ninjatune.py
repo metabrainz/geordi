74ce667df3ef572566c9ca6a8fae9ca0acc5894b
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2014-03-28 14:11:52 -0700

    ninjatune: cleanup old commented-out bits, document barcode brokenness

--- geordi/geordi/data/mapping/indexes/ninjatune.py
+++ geordi/geordi/data/mapping/indexes/ninjatune.py
@@ -31,8 +31,8 @@ ninjatune = {
                    both(lambda x, *args, **kwargs: ['release', 'labels', 'combined', (kwargs.get('index')+1,), SimplePathPart('label', no_manip=True)], 'SUB_LABEL', 'Sub_Label'),
                    both(lambda x, *args, **kwargs: ['release', 'labels', 'combined', (kwargs.get('index')+1,), SimplePathPart('catalog_number', no_manip=True)], 'CATALOGUE NUMBER', 'Catalogue Number'),
 
+                   # XXX: sometimes non-numeric, need to handle this case
                    both(['release', 'barcode'], 'BARCODE', 'Barcode', transform=lambda val, *args, **kwargs: str(int(val))),
-                   #both(['release', 'release_group'], 'CATALOGUE NUMBER', 'Catalogue Number', link=lambda value, data, *args, **kwargs: 'ninjatune/release/%s:rg' % value, link_only=True),
                    both(['release', 'tag'], 'MAIN GENRE', 'Main Genre', 'SUB_GENRE', 'Sub_Genre', condition=lambda x, *args, **kwargs: x != ''),
 
                    both(lambda x, *args, **kwargs: ['release', 'mediums', 'split', 'tracks', (kwargs.get('t_index'),), 'number'], 'TRACK NUMBER', 'track number',
