444395b552b1d57fd4799a068f4134f3deb9cf98
Roman Tsukanov <roman@tsukanov.me>
2014-07-08 21:00:33 +1000

    Implemented GeordiTestCase to simplify testing. Added testing-specific config file sample.

---
+++ geordi/geordi/test_case.py
@@ -0,0 +1,18 @@
+from flask.ext.testing import TestCase
+from geordi import create_app
+from . import db
+
+
+class GeordiTestCase(TestCase):
+
+    def create_app(self):
+        app = create_app('test.cfg')
+        app.config['TESTING'] = True
+        return app
+
+    def setUp(self):
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
