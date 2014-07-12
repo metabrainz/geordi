61f5fca340f77540cb736af39ad78b5ca99a9269
Roman Tsukanov <roman@tsukanov.me>
2014-07-13 01:30:23 +1000

    Added API endpoint for linked items.

--- geordi/geordi/api/views.py
+++ geordi/geordi/api/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, abort, jsonify
 from geordi.data.model.item import Item
 from geordi.data.model.item_data import ItemData
+from geordi.data.mapping.extract import extract_value
+import re
 
 api = Blueprint('api', __name__)
 
@@ -14,6 +16,34 @@ def item_data(item_id):
     if item is None:
         abort(404)
     return jsonify(item.to_dict())
+
+@api.route('/item/<int:item_id>/links')
+def item_links(item_id):
+    """Get links to specified item.
+
+    :resheader Content-Type: *application/json*
+    """
+    item = Item.get(item_id)
+    if item is None:
+        abort(404)
+
+    linked_items = {}
+    for item_link in set(item.items_linked):
+        linked_items[item_link.item_id] = Item.get(item_link.item_id)
+
+    def to_int_conditionally(val):
+        return int(val) if re.match('^\d+$', val) else val
+
+    links = []
+    for item_link in item.items_linked:
+        path = [to_int_conditionally(x) for x in item_link.type.split('%')]
+        links.append(dict(
+            item_id=item_link.item_id,
+            path=item_link.type,
+            value=extract_value(linked_items[item_link.item_id].map_dict, path)[0][1],
+        ))
+
+    return jsonify(links=links)
 
 @api.route('/data')
 def list_indexes():
