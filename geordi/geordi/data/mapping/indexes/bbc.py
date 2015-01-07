6e7e092b1a2e4111251ec802a4fd059f4c64bd56
Ian McEwen <ianmcorvidae@ianmcorvidae.net>
2015-01-07 12:49:41 -0700

    Add links for work composers in BBC data.

--- geordi/geordi/data/mapping/indexes/bbc.py
+++ geordi/geordi/data/mapping/indexes/bbc.py
@@ -1,21 +1,35 @@
 import re
 from ..rule import Rule
+from ..insert import SimplePathPart
 
-def make_artist_name(val, data, *args, **kwargs):
-    names = []
-    for prop in ['name_prefix', 'first_names', 'last_name']:
-        if data.get(prop) and data[prop] != '':
-            names.append(data[prop])
-    if len(names) > 0:
-        return " ".join(names)
-    else:
-        return ""
+
+def make_artist_name(**kwargs):
+    prefix = kwargs.get('prefix')
+    def closure(val, data, *args, **kwargs):
+        if prefix is not None and prefix in data:
+            data = data[prefix]
+        names = []
+        for prop in ['name_prefix', 'first_names', 'last_name']:
+            if data.get(prop) and data[prop] != '':
+                names.append(data[prop])
+        if len(names) > 0:
+            return " ".join(names)
+        else:
+            return ""
+
+    return closure
 
 bbc_index = {
     'artist': [
-        Rule(['first_names'], ['artist', 'name'], transform=make_artist_name)
+        Rule(['first_names'], ['artist', 'name'], transform=make_artist_name())
     ],
     'work': [
-        Rule(['name'], ['work', 'name'])
+        Rule(['name'], ['work', 'name']),
+        Rule(
+            ['composer', 'first_names'],
+            lambda *args, **kwargs: ['work', 'composers', (kwargs.get('index'),), SimplePathPart('target', no_manip=True)],
+            transform=make_artist_name(prefix='composer'),
+            link=lambda value, data, *args, **kwargs: 'bbc/artist/%s' % data['composer']['id'],
+        )
     ]
 }
